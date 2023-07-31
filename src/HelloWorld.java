414a27f0f6c18c4f006559c3acc61e0b6414488a
Donald Twitty <donaldctwittyjr@outlook.com>
2023-07-31 10:49:50 -0500

    completed Syntax, Data Types, and Variables Exercise

---
+++ src/HelloWorld.java
@@ -0,0 +1,56 @@
+import java.sql.SQLOutput;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.print("Hello, World!");
+        //System is a class//out is a method//
+        //Typing sout, is a shortcut for System.out.println//
+        System.out.print("\n");
+        System.out.print("Hello, ");
+        System.out.println("World!");
+
+//        int myFavoriteNumber = 5;
+//        System.out.println(myFavoriteNumber);
+//
+//        String myString = "3.14159";
+//        System.out.println(myString);
+//
+//        long float = 3;
+//        System.out.println(float);
+//
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+
+//        class ourHouse = 250_000;
+//        System.out.println(ourHouse);
+
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) o;
+//
+//        int three = (int) "three";
+
+//        int x = 4;
+//        x += 5;
+
+//        int x = 3;
+//        int y = 4;
+//        y *= x;
+
+        int x = 10;
+        int y = 2;
+        x /= y;
+        y -= x;
+        System.out.println(x);
+        System.out.println(y);
+
+
+
+    }
+}
+
