9c2b1a094dc0d1ed33f21862557b5e68de09b9e9
Donald Twitty <donaldctwittyjr@outlook.com>
2023-08-01 12:23:43 -0500

    String Exercise 1.String Basics

--- src/ConsoleExercise.java
+++ src/ConsoleExercise.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class ConsoleExercise {
     public static void main(String[] args) {
-//        double pi = 3.14159;
+        double pi = 3.14159;
 //        System.out.println("The value of pi is approximately " + String.format("%.2f", pi) + ".");
 //        System.out.format("The value of pi is approximately %.2f.%n", pi);
 
@@ -47,25 +47,25 @@ public class ConsoleExercise {
 //        scanner.close();
 
         //Bonus//
-        scanner.useDelimiter("\n"); // Set delimiter to newline
-
-        System.out.print("Enter the length of the classroom: ");
-        double length = scanner.nextDouble();
-
-        System.out.print("Enter the width of the classroom: ");
-        double width = scanner.nextDouble();
-
-        System.out.print("Enter the height of the classroom: ");
-        double height = scanner.nextDouble();
-
-        double area = length * width;
-        double perimeter = 2 * (length + width);
-        double volume = length * width * height;
-
-        System.out.println("The area of the classroom is: " + area);
-        System.out.println("The perimeter of the classroom is: " + perimeter);
-        System.out.println("The volume of the classroom is: " + volume);
-
-        scanner.close();
+//        scanner.useDelimiter("\n"); // Set delimiter to newline
+//
+//        System.out.print("Enter the length of the classroom: ");
+//        double length = scanner.nextDouble();
+//
+//        System.out.print("Enter the width of the classroom: ");
+//        double width = scanner.nextDouble();
+//
+//        System.out.print("Enter the height of the classroom: ");
+//        double height = scanner.nextDouble();
+//
+//        double area = length * width;
+//        double perimeter = 2 * (length + width);
+//        double volume = length * width * height;
+//
+//        System.out.println("The area of the classroom is: " + area);
+//        System.out.println("The perimeter of the classroom is: " + perimeter);
+//        System.out.println("The volume of the classroom is: " + volume);
+//
+//        scanner.close();
     }
 }
