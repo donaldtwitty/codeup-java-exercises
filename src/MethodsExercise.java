5dfae58696e6b4b5737f2a6fe26c60e8df3403dc
Donald Twitty <donaldctwittyjr@outlook.com>
2023-08-02 16:45:08 -0500

    methods exercise problem 4 completed

--- src/MethodsExercise.java
+++ src/MethodsExercise.java
@@ -3,24 +3,24 @@ import java.util.Scanner;
 public class MethodsExercise {
 
     // Number 1 //
-    public static int addition(int num1, int num2) {
-        return num1 + num2;
-    }
-
-    public static int subtraction(int num1, int num2) {
-        return num1 - num2;
-    }
-
-    public static int multiplication(int num1, int num2) {
-        return num1 * num2;
-    }
-
-    public static double division(double num1, double num2) {
-        if (num2 == 0) {
-            throw new ArithmeticException("Cannot divide by zero");
-        }
-        return num1 / num2;
-    }
+//    public static int addition(int num1, int num2) {
+//        return num1 + num2;
+//    }
+//
+//    public static int subtraction(int num1, int num2) {
+//        return num1 - num2;
+//    }
+//
+//    public static int multiplication(int num1, int num2) {
+//        return num1 * num2;
+//    }
+//
+//    public static double division(double num1, double num2) {
+//        if (num2 == 0) {
+//            throw new ArithmeticException("Cannot divide by zero");
+//        }
+//        return num1 / num2;
+//    }
 
 //    public static void main(String[] args) {
 //        int num1 = 10;
@@ -61,7 +61,7 @@ public class MethodsExercise {
 
     // Using recursion: //
 
-//        public static int multiplicationWithRecursion(int num1, int num2) {
+    //        public static int multiplicationWithRecursion(int num1, int num2) {
 //            if(num2 == 0) {
 //                return 0;
 //            }
@@ -77,9 +77,9 @@ public class MethodsExercise {
 //            System.out.println("Multiplication with recursion: " + multiplicationWithRecursion(num1, num2));
 //        }
 //    }
-            // Number 2 //
+    // Number 2 //
 //    public static int getInteger(int min, int max) {
-        Scanner scanner = new Scanner(System.in);
+//    Scanner scanner = new Scanner(System.in);
 //        System.out.printf("Enter a number between %d and %d: ", min, max);
 //        int input = scanner.nextInt();
 //
@@ -95,31 +95,61 @@ public class MethodsExercise {
 //        int userInput = getInteger(1, 10);
 //        System.out.printf("You entered %d.%n", userInput);
 
-        // Number 3 //
-        public static void main(String[] args) {
-            Scanner scanner = new Scanner(System.in);
-            boolean continueProgram = true;
+    // Number 3 //
+//    public static void main(String[] args) {
+//        Scanner scanner = new Scanner(System.in);
+//        boolean continueProgram = true;
+//
+//        while (continueProgram) {
+//
+//            int userInput;
+//            do {
+//                System.out.print("Enter an integer from 1 to 10: ");
+//                userInput = scanner.nextInt();
+//            } while (userInput < 1 || userInput > 10);
+//
+//            long factorial = 1;
+//            for (int i = 1; i <= userInput; i++) {
+//                factorial *= i;
+//            }
+//
+//            System.out.printf("%d! = %d%n", userInput, factorial);
+//
+//            System.out.print("Do you want to continue? (y/n): ");
+//            String continueChoice = scanner.next();
+//            continueProgram = continueChoice.equalsIgnoreCase("y");
+//        }
+//    }
 
-            while (continueProgram) {
-
-                int userInput;
-                do {
-                    System.out.print("Enter an integer from 1 to 10: ");
-                    userInput = scanner.nextInt();
-                } while (userInput < 1 || userInput > 10);
-
-                long factorial = 1;
-                for (int i = 1; i <= userInput; i++) {
-                    factorial *= i;
-                }
-
-                System.out.printf("%d! = %d%n", userInput, factorial);
-
-                System.out.print("Do you want to continue? (y/n): ");
-                String continueChoice = scanner.next();
-                continueProgram = continueChoice.equalsIgnoreCase("y");
+    // Number 4 //
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int numSides;
+        do {
+            System.out.print("Enter the number of sides for the dice: ");
+            numSides = scanner.nextInt();
+            if (numSides <= 0) {
+                System.out.println("Invalid number of sides. Try again.");
             }
+        } while (numSides <= 0);
+
+        boolean rollAgain = true;
+        while (rollAgain) {
+            int roll1 = rollDie(numSides);
+            int roll2 = rollDie(numSides);
+            System.out.printf("You rolled a %d and a %d.%n", roll1, roll2);
+
+            System.out.print("Do you want to roll the dice again? (y/n)");
+            String answer = scanner.next();
+            rollAgain = answer.equalsIgnoreCase("y");
         }
     }
+
+    private static int rollDie(int numSides) {
+        return (int) (Math.random() * numSides) + 1;
+    }
+}
+
+
 
 
